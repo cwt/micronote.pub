41c763679e40a023a55b653fba09b5f512861740
Chaiwat Suttipongsakul <cwt@bashell.com>
2019-01-26 17:27:02 +0700

    add support for alias ':blob' without underscore

--- app.py
+++ app.py
@@ -281,6 +281,7 @@ def clean(html):
 def flexmoji(html):
     html = emoji.emojize(html, use_aliases=True)
     html = emoji.emojize(html, use_aliases=True, delimiters=(':blob_', ':'))
+    html = emoji.emojize(html, use_aliases=True, delimiters=(':blob', ':'))
     return html
 
 
