ec4a1864659e20284a063391ec3bbc298fb371aa
Chaiwat Suttipongsakul <cwt@bashell.com>
2019-01-27 22:35:30 +0700

    change gunicorn log level to info

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /opt/ublog/bin/python -O -c "import config; config.create_indexes()"
-/opt/ublog/bin/gunicorn -t 300 -w 2 -k gevent -b 0.0.0.0:5005 --log-level debug app:app
+/opt/ublog/bin/gunicorn -t 300 -w 2 -k gevent -b 0.0.0.0:5005 --log-level info app:app
